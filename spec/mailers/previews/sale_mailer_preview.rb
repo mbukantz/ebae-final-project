9947d513ad87c71ad1f356322479521d63cfad3a
r0mg <roslynnmg@gmail.com>
2015-12-03 18:49:45 -0500

    added previews for seller and buyer emails when new sale made

--- spec/mailers/previews/sale_mailer_preview.rb
+++ spec/mailers/previews/sale_mailer_preview.rb
@@ -1,4 +1,12 @@
 # Preview all emails at http://localhost:3000/rails/mailers/sale_mailer
 class SaleMailerPreview < ActionMailer::Preview
 
+  def seller_mail_preview
+    SaleMailer.seller_email(User.first)
+  end
+
+  def buyer_mail_preview
+    SaleMailer.buyer_email(User.first)
+  end
+
 end
