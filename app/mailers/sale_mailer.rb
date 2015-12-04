1a22b5704879210ec86cd4a64ec98404e95a0cd4
r0mg <roslynnmg@gmail.com>
2015-12-03 20:08:37 -0500

    pulls in buyer demographic info into seller invoice email

--- app/mailers/sale_mailer.rb
+++ app/mailers/sale_mailer.rb
@@ -1,10 +1,12 @@
 class SaleMailer < ApplicationMailer
   default from: "ebae.auctions@gmail.com"
 
-  def seller_email(listing)
-    @listing = listing
-    @seller = listing.seller.user
-    mail(to: @seller.email, subject: 'New invoice created')
+  def seller_email(sale)
+    @sale = sale
+    @listing = sale.listing
+    @seller = sale.listing.seller.user
+    @buyer = sale.buyer.user
+    mail(to: @seller.email, from: 'bae@ebae.com', subject: 'New invoice created')
   end
 
   def buyer_email(user)
--- spec/mailers/previews/sale_mailer_preview.rb
+++ spec/mailers/previews/sale_mailer_preview.rb
@@ -2,7 +2,7 @@
 class SaleMailerPreview < ActionMailer::Preview
 
   def seller_mail_preview
-    SaleMailer.seller_email(Listing.first)
+    SaleMailer.seller_email(Sale.first)
   end
 
   def buyer_mail_preview
