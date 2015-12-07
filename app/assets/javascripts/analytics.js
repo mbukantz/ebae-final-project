8df800e361f20b4fad0f7beb5a759323934cf444
hwinn4 <hilary.winn@gmail.com>
2015-12-06 21:36:24 -0500

    renders bars graph

--- app/assets/javascripts/analytics.js
+++ app/assets/javascripts/analytics.js
@@ -1,45 +1,34 @@
 $(function(){
 
+ 
 
+  var makeBarGraph = function(data){
+       // debugger;
+    var chart = d3.select('#bar-chart-js');
+    var bar = chart.selectAll('div');
+    var barLength = bar.data(data);
+    var addBar = barLength.enter().append('div').attr('class', 'bar');
+    addBar.style('width', function(d){
+      return d*10 + 'px';
+    });
+    addBar.text(function(d){
+      return d;
+    })
+  }
   // var data = [1, 4, 3, 2];
   // debugger;
   
-  var data =  function returnData(data){
-      return data;
-    }
-  getCategories();  
-       // $.getJSON('/analytics.json', function(response){
-       //    var categories_count = response.categories_count;
-       //    var counts = []
-       //    // var categoriesObj = {}
-       //    // for (var i = 0; i < categories_count.length; i++){
-       //    //   categoriesObj[categories_count[i][0]] = categories_count[i][1];
-       //    // }
-       //    for (var i = 0; i < categories_count.length; i++){
-       //      counts.push(categories_count[i][1]);
-       //    }
-       //    // debugger;
-       //  returnData(counts);
-       //  })
+
 
   // debugger;
   // var x = d3.scale.linear()
   //     .domain([0, d3.max(data)])
   //     .range([0, 40]);
-  var chart = d3.select('#bar-chart-js');
-  var bar = chart.selectAll('div');
-  var barLength = bar.data(data);
-  var addBar = barLength.enter().append('div').attr('class', 'bar');
-  addBar.style('width', function(d){
-    return x(d) + 'px';
-  });
-  addBar.text(function(d){
-    return d;
-  })
+  
 
    
 
-   function getCategories(){
+  var getCategories = function(cb){
       $.getJSON('/analytics.json', function(response){
         var categories_count = response.categories_count;
         var counts = []
@@ -50,12 +39,13 @@ $(function(){
         for (var i = 0; i < categories_count.length; i++){
           counts.push(categories_count[i][1]);
         }
-        returnData(counts);
+
+        cb(counts); // callback
       })
     }
 
 
-  
+  getCategories(makeBarGraph);
 
 
 
