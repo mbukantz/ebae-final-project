f033ab9ede7092f0ce1d6040d328695c1842d2e1
r0mg <roslynnmg@gmail.com>
2015-12-08 16:05:42 -0500

    sends email when new item watched

--- app/controllers/watchlists_controller.rb
+++ app/controllers/watchlists_controller.rb
@@ -5,12 +5,14 @@ class WatchlistsController < ApplicationController
       redirect_to listing_path(params[:listing_id])
     else 
       watchlist = Watchlist.create(buyer_id: current_user.id, listing_id: params[:listing_id])
+      WatchlistMailer.added_item(watchlist).deliver_now
       redirect_to listing_path(params[:listing_id])
     end
   end
 
   def destroy 
     watchlist = Watchlist.find_watchlist(current_user.id, params[:listing_id])
+    WatchlistMailer.removed_item(watchlist).deliver_now
     Watchlist.destroy(watchlist.first.id)
     redirect_to dashboard_index_path
   end
--- app/mailers/watchlist_mailer.rb
+++ app/mailers/watchlist_mailer.rb
@@ -1,2 +1,19 @@
 class WatchlistMailer < ApplicationMailer
+  default from: "ebae.auctions@gmail.com"
+
+  def added_item(watchlist)
+    @watchlist = watchlist
+    @listing = watchlist.listing
+    @watcher = watchlist.buyer.user
+    mail(to: @watcher.email, 
+        from: 'bae@ebae.com', 
+        subject: "Now watching #{@listing.item.name}")
+  end
+
+  def removed_item(watchlist)
+  end
+
+  def item_ending(watchlist)
+  end
+
 end
