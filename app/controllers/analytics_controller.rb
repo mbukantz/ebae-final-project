5e765d4a8f07e627d8110512779bdfdb23b5da45
r0mg <roslynnmg@gmail.com>
2015-12-08 11:49:09 -0500

    adds highest price by month and fixes dashboard current bid thing

---
+++ app/controllers/analytics_controller.rb
@@ -0,0 +1,16 @@
+class AnalyticsController < ApplicationController
+
+ def index
+
+   # @analytic = AnalyticViewObject.new
+
+   respond_to do |format|
+     format.html 
+     format.json { 
+       @categories_count = @analytic.auctions_by_category
+       render json: {categories_count: @categories_count}
+     }
+   end
+ end
+
+end
