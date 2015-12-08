08f78ebc5e0403f35817c718de6720cdabdbc658
hwinn4 <hilary.winn@gmail.com>
2015-12-08 14:47:33 -0500

    revises schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20151208162614) do
 
   create_table "advanced_searches", force: :cascade do |t|
