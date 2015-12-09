1653020144ea1d5a7fb73de5d146e43f34b386d4
hwinn4 <hilary.winn@gmail.com>
2015-12-09 17:53:49 -0500

    renames variables in auctions_by_category

--- app/view_objects/analytic_view_object.rb
+++ app/view_objects/analytic_view_object.rb
@@ -20,9 +20,9 @@ class AnalyticViewObject
   def auctions_by_category
    category_count = Item.group(:category_id).count
     # => {1=>1, 4=>4, 5=>3, 7=>2}
-   category_count_with_names = Category.all.each_with_object({}) do | category_obj, category_count_hash|
-     if category_count[category_obj.id]
-       category_count_hash[category_obj.name] = category_count[category_obj.id]
+   category_count_with_names = Category.all.each_with_object({}) do | category, category_count_hash|
+     if category_count[category.id]
+       category_count_hash[category.name] = category_count[category.id]
      end
    end
    category_count_with_names.sort_by{|k, v| v}.reverse.to_h
