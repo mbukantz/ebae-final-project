2bbfc2ae2a7e15f3cf4231879fc6c10780ff5674
hwinn4 <hilary.winn@gmail.com>
2015-12-11 22:15:08 -0500

    deletes api files

--- app/serializers/item_serializer.rb
+++
@@ -1,5 +0,0 @@
-class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :condition_id, :listing_id, :category_id
-  has_one :listing
-  has_one :condition
-end
--- config/routes.rb
+++ config/routes.rb
@@ -31,7 +31,5 @@ Rails.application.routes.draw do
   get '/logout', to: 'sessions#destroy', as: 'log_out'
   root to: "home#index"
   get '/analytics', to: 'analytics#index', as: 'analytics'
-  namespace :api, :defaults => { :format => 'json' } do 
-    resources :categories
-  end
+
 end
